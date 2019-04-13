fcc76b6aa68f44e5d61684ddeac1037975a1b03d
Sergey Andryunin <admin@sergeyvisman.ru>
2019-04-13 15:56:18 +0300

    Update delete_by_id.py

--- delete_by_id.py
+++ delete_by_id.py
@@ -1,5 +1,7 @@
 import vk
 import os
+import webbrowser
+from urllib.parse import parse_qs
 
 """
 To use vk.com API you need registered app and login in this social network. vk.com developers guide: https://vk.com/dev/main
@@ -11,13 +13,26 @@ To use vk.com API you need registered app and login in this social network. vk.c
 5. Use this access token to make method requests. List of all: https://vk.com/dev/methods. Some methods don’t require access token.
 """
 
-# сюда вставляем свой токен и id группы откуда удаляем
+# сюда вставляем app_id и id группы откуда удаляем
 
-VK_TOKEN = ''
 GROUP_ID = 1
+APP_ID = 1
 
 
-def get_auth_api(access_token):
+def get_auth_params():
+    auth_url = ("https://oauth.vk.com/authorize?client_id={app_id}"
+                "&scope=groups&redirect_uri=http://oauth.vk.com/blank.html"
+                "&display=page&response_type=token&v=5.95".format(app_id=APP_ID))
+    webbrowser.open_new_tab(auth_url)
+    redirected_url = input("В браузере в адресной строке появилась ссылка, вставьте ее сюда:\n")
+    aup = parse_qs(redirected_url)
+    aup['access_token'] = aup.pop(
+        'https://oauth.vk.com/blank.html#access_token')
+    return aup['access_token'][0]
+
+
+def get_auth_api():
+    access_token = get_auth_params()
     session = vk.Session(access_token=access_token)
     return vk.API(session)
 
@@ -40,11 +55,11 @@ def main():
 
     :return:
     """
-    vk_api = get_auth_api(VK_TOKEN)
+    vk_api = get_auth_api()
     group_id = GROUP_ID
     id_to_remove = read_from_file()
     for user_id in id_to_remove:
-        response = vk_api.groups.removeUser(group_id=group_id, user_id=user_id)
+        response = vk_api.groups.removeUser(group_id=group_id, user_id=int(user_id), v='5.95')
         if response:
             print('Пользователь {id} успешно удален'.format(id=user_id))
         else:
